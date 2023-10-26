1333c1f6118e6eb139059f9dc8947e43653fc3cb
Chhan Bunthong <chhan.bunthong@sathapana.com.kh>
2023-10-26 22:20:45 +0700

    update: role store

---
+++ src/stores/constant.ts
@@ -0,0 +1,1 @@
+export declare type TypeStatus = 'INIT' | 'PENDING' | 'FAILED' | 'COMPLETED'
--- src/stores/role/slice.ts
+++ src/stores/role/slice.ts
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getRoleCreate, getRoleDelete, getRoleDetail, getRoleList, getRoleUpdate } from "./action";
+import { TypeStatus } from "stores/constant";
 
 interface IRole {
     form: {
-        isProcessing: boolean
+        isLoading: boolean
     }
     create: {
         isLoading: boolean,
@@ -21,7 +22,7 @@ interface IRole {
         error: any
     }
     list: {
-        isLoading: boolean,
+        status: TypeStatus
         data: any,
         error: any,
         metaData: {
@@ -31,19 +32,19 @@ interface IRole {
         }
     }
     detail: {
-        isLoading: boolean,
+        status: TypeStatus
         data: any,
         error: any
     }
 }
 
 const initialState: IRole = {
-    form: { isProcessing: false },
+    form: { isLoading: false },
     create: { isLoading: false, data: null, error: null },
     update: { isLoading: false, data: null, error: null },
-    detail: { isLoading: false, data: null, error: null },
     delete: { isLoading: false, data: null, error: null },
-    list: { isLoading: false, data: [], error: null, metaData: { skip: 0, limit: 10, total: 0 } },
+    detail: { status: 'INIT', data: null, error: null },
+    list: { status: 'INIT', data: [], error: null, metaData: { skip: 0, limit: 10, total: 0 } },
 }
 
 const roleSlice = createSlice({
@@ -53,63 +54,63 @@ const roleSlice = createSlice({
     extraReducers(builder) {
         builder.addCase(getRoleCreate.pending, (state) => {
             state.create.isLoading = true
-            state.form.isProcessing = true
+            state.form.isLoading = true
         })
         builder.addCase(getRoleCreate.rejected, (state, action) => {
             state.create.isLoading = false
-            state.form.isProcessing = false
+            state.form.isLoading = false
             state.create.error = action.payload?.response?.data
         })
         builder.addCase(getRoleCreate.fulfilled, (state, action) => {
             state.create.error = null
             state.create.isLoading = false
-            state.form.isProcessing = false
+            state.form.isLoading = false
             state.create.data = action.payload
         })
 
         // Delete
         builder.addCase(getRoleDelete.pending, (state) => {
             state.delete.isLoading = true
-            state.form.isProcessing = true
+            state.form.isLoading = true
         })
         builder.addCase(getRoleDelete.rejected, (state, action) => {
             state.delete.isLoading = false
-            state.form.isProcessing = false
+            state.form.isLoading = false
             state.delete.error = action.payload?.response?.data
         })
         builder.addCase(getRoleDelete.fulfilled, (state, action) => {
             state.delete.error = null
             state.delete.isLoading = false
-            state.form.isProcessing = false
+            state.form.isLoading = false
             state.delete.data = action.payload
         })
 
         // List
         builder.addCase(getRoleList.pending, (state) => {
-            state.list.isLoading = true
+            state.list.status = 'PENDING'
         })
         builder.addCase(getRoleList.rejected, (state, action) => {
-            state.list.isLoading = false
+            state.list.status = 'FAILED'
             state.list.error = action.payload?.response?.data
         })
         builder.addCase(getRoleList.fulfilled, (state, action) => {
             state.list.error = null
-            state.list.isLoading = false
+            state.list.status = 'COMPLETED'
             state.list.data = action.payload?.data
             state.list.metaData = action.payload?.metaData
         })
 
         // Detail
         builder.addCase(getRoleDetail.pending, (state) => {
-            state.detail.isLoading = true
+            state.detail.status = 'PENDING'
         })
         builder.addCase(getRoleDetail.rejected, (state, action) => {
-            state.detail.isLoading = false
+            state.detail.status = 'FAILED'
             state.detail.error = action.payload?.response?.data
         })
         builder.addCase(getRoleDetail.fulfilled, (state, action) => {
             state.detail.error = null
-            state.detail.isLoading = false
+            state.detail.status = 'COMPLETED'
             state.detail.data = action.payload?.data
         })
 
