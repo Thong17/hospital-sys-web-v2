8cf0207322b0f323067e5eb354ccab7d177046a3
Chhan Bunthong <chhan.bunthong@sathapana.com.kh>
2023-12-05 10:35:40 +0700

    update: pirivlege container

--- src/components/shared/containers/PrivilegeContainer.tsx
+++ src/components/shared/containers/PrivilegeContainer.tsx
@@ -4,7 +4,7 @@ import { SectionContainer } from './SectionContainer'
 import ShortcutRoundedIcon from '@mui/icons-material/ShortcutRounded'
 import ItemContainer from './ItemContainer'
 import useTheme from 'hooks/useTheme'
-import { renderColor } from 'utils/index'
+import { checkAllFieldObject, renderColor } from 'utils/index'
 
 const PrivilegeContainer = ({
   navigation = {},
@@ -17,55 +17,76 @@ const PrivilegeContainer = ({
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
-      {Object.keys(navigation)?.length > 0
-        ? Object.keys(navigation)?.map((nav, key) => {
-            return (
-              <SectionContainer
-                key={key}
-                label={`${translate(nav?.toUpperCase())}`}
-              >
-                <Stack direction={'column'} gap={3} ml={1}>
-                  {Object.keys(navigation[nav])?.map((sub, key) => {
-                    return (
-                      <Box key={key}>
-                        <Stack direction={'row'} gap={1}>
-                          <ShortcutRoundedIcon
-                            sx={{ transform: 'scaleY(-1)', color: theme.text.tertiary }}
-                          />
-                          <Typography sx={{ color: theme.text.tertiary }}>
-                            {translate(sub?.toUpperCase())}
-                          </Typography>
-                        </Stack>
-                        <Box
+      {Object.keys(navigation)?.length > 0 ? (
+        Object.keys(navigation)?.map((nav, key) => {
+          return (
+            <SectionContainer
+              key={key}
+              label={`${translate(nav?.toUpperCase())}`}
+              sx={{
+                display: checkAllFieldObject(navigation[nav])
+                  ? 'block'
+                  : 'none',
+              }}
+            >
+              <Stack direction={'column'} gap={3} ml={1}>
+                {Object.keys(navigation[nav])?.map((sub, key) => {
+                  return (
+                    <Box
+                      key={key}
+                      sx={{
+                        display:
+                          Object.keys(privilege[nav]?.[sub] || {}).length > 0 &&
+                          checkAllFieldObject(privilege[nav]?.[sub])
+                            ? 'block'
+                            : 'none',
+                      }}
+                    >
+                      <Stack direction={'row'} gap={1}>
+                        <ShortcutRoundedIcon
                           sx={{
-                            display: 'grid',
-                            gridTemplateColumns:
-                              'repeat(auto-fill, minmax(140px, 1fr))',
-                            padding: '10px 40px',
-                            gap: '10px',
+                            transform: 'scaleY(-1)',
+                            color: theme.text.tertiary,
                           }}
-                        >
-                          {Object.keys(privilege[nav]?.[sub] || {}).length > 0
-                            ? Object.keys(privilege[nav]?.[sub] || {}).map(
-                                (action, key) => (
-                                  <ItemContainer
-                                    text={translate(action.toUpperCase())}
-                                    key={key}
-                                    color={renderColor(action, theme)}
-                                    sx={{ display: privilege[nav]?.[sub]?.[action] ? 'block' : 'none' }}
-                                  />
-                                )
-                              )
-                            : <Typography>{translate('NO_ACTION')}</Typography>}
-                        </Box>
+                        />
+                        <Typography sx={{ color: theme.text.tertiary }}>
+                          {translate(sub?.toUpperCase())}
+                        </Typography>
+                      </Stack>
+                      <Box
+                        sx={{
+                          display: 'grid',
+                          gridTemplateColumns:
+                            'repeat(auto-fill, minmax(140px, 1fr))',
+                          padding: '10px 40px',
+                          gap: '10px',
+                        }}
+                      >
+                        {Object.keys(privilege[nav]?.[sub] || {}).map(
+                          (action, key) => (
+                            <ItemContainer
+                              text={translate(action.toUpperCase())}
+                              key={key}
+                              color={renderColor(action, theme)}
+                              sx={{
+                                display: privilege[nav]?.[sub]?.[action]
+                                  ? 'block'
+                                  : 'none',
+                              }}
+                            />
+                          )
+                        )}
                       </Box>
-                    )
-                  })}
-                </Stack>
-              </SectionContainer>
-            )
-          })
-        : <Typography>{translate('NO_PRIVILEGE')}</Typography>}
+                    </Box>
+                  )
+                })}
+              </Stack>
+            </SectionContainer>
+          )
+        })
+      ) : (
+        <Typography>{translate('NO_PRIVILEGE')}</Typography>
+      )}
     </Box>
   )
 }
